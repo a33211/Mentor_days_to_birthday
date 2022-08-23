5b2c6c3f97ff5c1a7329ea9eb11593274f072960
Alex <a33211noskov@gmail.com>
2022-08-23 19:56:06 +0300

    Initial task working

--- main.py
+++ main.py
@@ -10,24 +10,22 @@ class Person(PersonKeep):
 
     def __init__(self, name, birthday) -> str:
         self.name = name
-        self.birthday = birthday
+        self.birthday = datetime.strptime(birthday, '%d.%m.%Y').date()
 
-    def days_to_birthday(self, birthday):
+    def days_to_birthday(self):
         current = datetime.now().date()
-        birthday_parsed = datetime.strptime(birthday, '%d.%m.%Y').date()
-        birthday_parsed_modified = birthday_parsed.replace(current.year)
+        birthday_parsed_modified = self.birthday.replace(current.year)
         if birthday_parsed_modified > current:
             delta = birthday_parsed_modified - current
         else:
             next_birthday = birthday_parsed_modified.replace(current.year + 1)
             delta = next_birthday - current
-        return delta
-        print(f'you have {delta} days before birthday')
+        return delta.days
         # if datetime.MAXYEAR > current(year)
 
 
 if __name__ == '__main__':
     person_A = Person('Andrew', '10.08.2020')
     person_B = Person('Benjamin', '02.02.1980')
-    print(person_A.days_to_birthday())
-    print(person_B.days_to_birthday())
+    print(f'{person_A.name} has {person_A.days_to_birthday()} days left')
+    print(f'{person_B.name} has {person_B.days_to_birthday()} days left')
